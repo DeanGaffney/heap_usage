50176c08a2905b840cfb6d1588b034d85251a546
Dean Gaffney <gaffmasterflex123@gmail.com>
2016-03-23 10:54:14 +0000

    Made wordpairs comparable by ht length of the cmbined string of the spanish and english words,for the max heap comparisons.

--- src/models/WordPair.java
+++ src/models/WordPair.java
@@ -1,7 +1,8 @@
 package models;
 
-public class WordPair {
+public class WordPair implements Comparable<WordPair> {
 
+	private int pairLength; // used to compare two objects
 	private String spanishWord;
 	private String englishWord;
 	
@@ -17,9 +18,21 @@ public class WordPair {
 	public String getSpanishword(){
 		return spanishWord;
 	}
+	
+	public int getpairLength(){
+		return spanishWord.length() + englishWord.length();
+	}
 
 	@Override
 	public String toString() {
 		return "WordPair [spanishWord=" + spanishWord + ", englishWord=" + englishWord + "]";
 	}
+
+	@Override
+	public int compareTo(WordPair other) {
+		if(this.pairLength > other.pairLength)return +1;
+		if(this.pairLength < other.pairLength)return -1;
+		return 0;
+	}
+
 }
